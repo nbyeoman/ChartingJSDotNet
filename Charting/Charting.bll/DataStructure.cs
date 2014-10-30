39df429f4f894ddd758589ef5983ee3d9ddacfd1
nbyeoman <nyeoman@gmail.com>
2014-10-29 22:45:39 -0500

    Changed Namespaces.

--- Charting/Charting.bll/DataStructure.cs
+++ Charting/Charting.bll/DataStructure.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Charting.bll
+namespace ChartingObjects
 {
     public class DataStructure
     {
--- Charting/Charting.bll/Enums.cs
+++ Charting/Charting.bll/Enums.cs
@@ -1,4 +1,4 @@
-﻿namespace Charting.bll
+﻿namespace ChartingObjects
 {
 
     public enum DataPointValueType
